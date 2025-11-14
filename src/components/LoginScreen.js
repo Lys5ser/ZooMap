ab2df8401020796753e359d82d1cf1db230d6ac6
Lysandro Marques <lysandro.marques@lernende.bbw.ch>
2025-11-14 09:07:19 +0100

    add login

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import '../styles/LoginScreen.css';
-import StatusBar from './StatusBar';
 import LogoCard from './LogoCard';
 import LoginCard from './LoginCard';
-import HomeIndicator from './HomeIndicator';
 
 const LoginScreen = () => {
   return (
-    <div className="mobile-container">
-      <StatusBar />
-      <div className="app-content">
-        <div className="background-container">
-          <div className="gradient-bg"></div>
-          <div className="wildlife-overlay"></div>
-        </div>
+    <div className="desktop-container">
+      <div className="background-container">
+        <div className="gradient-bg"></div>
+        <div className="wildlife-overlay"></div>
+      </div>
+      <div className="content-wrapper">
         <LogoCard />
         <LoginCard />
       </div>
-      <HomeIndicator />
     </div>
   );
 };
